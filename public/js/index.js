00ef02780ec061e15ca0f749d242f0d065d054b0
Saeid Mohadjer <saeid@fastmail.fm>
2023-11-24 10:20:01 +0100

    refactor

--- public/js/index.js
+++ public/js/index.js
@@ -1,96 +1,21 @@
-import validate from './validate.js';
+import {
+  addSubmitListener,
+  fetchJson,
+  getFormData,
+  render,
+  renderProfile
+} from './lib.js';
 
+// cache DOM elements
 const $form = document.querySelector('#form');
-addSubmitListener($form);
+const $profile = document.querySelector('#profile');
 
+// generate form from json and display it on the page
+
+addSubmitListener($form, $profile);
 const userData = await fetchJson('/api/profile');
-
-// add form to the page
 const formData = await getFormData(userData);
 $form.innerHTML = await render('./template/form.hbs', formData);
 
-// display user data in profile section
-const $profile = document.querySelector('#profile');
+// populate profile with data and display it on the page
 renderProfile(userData, $profile);
-
-function addSubmitListener(form) {
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const data = new FormData(event.target);
-    const json = JSON.stringify(Object.fromEntries(data));
-
-    if (!validate(event.target)) {
-      return
-    }
-
-    fetch('/api/profile', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: json
-    })
-    .then((response) => response.json())
-    .then(async (json) => {
-      if (json.error) {
-        displayError(json.error);
-      } else {
-        renderProfile(json, $profile);
-      }
-    }).catch(function(err) {
-      console.error(`Error: ${err}`);
-    });
-  });
-}
-
-async function renderProfile(dbData, profileElement) {
-  profileElement.innerHTML = `<code>${JSON.stringify(dbData)}</code>`;
-}
-
-function displayError(error) {
-  const errors = error.errors;
-  if (Array.isArray(errors) && errors.length > 0) {
-    errors.forEach((errorObject) => {
-      const field = $form.querySelector(`input[name=${ errorObject.name}`);
-      if (field) {
-        field.classList.add('error');
-        field.nextElementSibling.textContent = errorObject.error;
-        field.nextElementSibling.removeAttribute('hidden');
-      }
-    });
-  }
-}
-
-async function getFormData(userData) {
-  const formJson = await fetchJson('./json/form.json');
-  // add value from database to form fields
-  if (userData.length) {
-    formJson.fields.map(field => {
-      const dbField = userData.find((item) => item.name === field.name);
-      return field.value = dbField.value;
-    });
-  }
-  return formJson;
-}
-
-// render function uses Handlebars library to populate a template with data and
-// return the result as a HTML string
-async function render(templatePath, data) {
-  const template = await fetchTemplate(templatePath);
-  const compiledTemplate = Handlebars.compile(template);
-  const htmlString = compiledTemplate(data);
-  return htmlString;
-}
-
-async function fetchTemplate(path) {
-  const response = await fetch(path);
-  const responseText = await response.text();
-  return responseText;
-}
-
-async function fetchJson(path) {
-  const response = await fetch(path);
-  const responseJson = await response.json();
-  return responseJson;
-}
---
+++ public/js/lib.js
@@ -0,0 +1,117 @@
+function validateData($form) {
+  const inputs = $form.querySelectorAll('input');
+  const state = { isValid: true };
+  const resetValidation = (inputs) => {
+    inputs.forEach((input) => {
+      input.classList.remove('error');
+      input.nextElementSibling.setAttribute('hidden', 'hidden');
+    });
+  };
+  const setError = (input) => {
+    state.isValid = false;
+    input.classList.add('error');
+    input.nextElementSibling.removeAttribute('hidden');
+  };
+  const formData = new FormData($form);
+  const validate = () => {
+    for (const item of formData.entries()) {
+      const inputField = $form.querySelector(`input[name="${item[0]}"`);
+      if (item[0] === 'age' && isNaN(item[1])) {
+        setError(inputField);
+      }
+
+      // to find out if a string contains number we use regix /\d/
+      if (item[0] !== 'age' && /\d/.test(item[1])) {
+        setError(inputField);
+      }
+    }
+  };
+
+  resetValidation(inputs);
+  validate();
+
+  return state.isValid;
+}
+
+export function addSubmitListener($form, $profile) {
+  $form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const json = JSON.stringify(Object.fromEntries(data));
+
+    if (!validateData(event.target)) {
+      return
+    }
+
+    fetch('/api/profile', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: json
+    })
+    .then((response) => response.json())
+    .then(async (json) => {
+      if (json.error) {
+        displayError($form, json.error);
+      } else {
+        renderProfile(json, $profile);
+      }
+    }).catch(function(err) {
+      console.error(`Error: ${err}`);
+    });
+  });
+}
+
+export async function renderProfile(dbData, profileElement) {
+  profileElement.innerHTML = `<code>${JSON.stringify(dbData)}</code>`;
+}
+
+export function displayError($form, error) {
+  const errors = error.errors;
+  if (Array.isArray(errors) && errors.length > 0) {
+    errors.forEach((errorObject) => {
+      const field = $form.querySelector(`input[name=${ errorObject.name}`);
+      if (field) {
+        field.classList.add('error');
+        field.nextElementSibling.textContent = errorObject.error;
+        field.nextElementSibling.removeAttribute('hidden');
+      }
+    });
+  }
+}
+
+export async function getFormData(userData) {
+  const formJson = await fetchJson('./json/form.json');
+  // add value from database to form fields
+  if (userData.length) {
+    formJson.fields.map(field => {
+      const dbField = userData.find((item) => item.name === field.name);
+      return field.value = dbField.value;
+    });
+  }
+  return formJson;
+}
+
+// render function uses Handlebars library to populate a template with data and
+// return the result as a HTML string
+export async function render(templatePath, data) {
+  const template = await fetchTemplate(templatePath);
+  const compiledTemplate = Handlebars.compile(template);
+  const htmlString = compiledTemplate(data);
+  return htmlString;
+}
+
+export async function fetchTemplate(path) {
+  const response = await fetch(path);
+  const responseText = await response.text();
+  return responseText;
+}
+
+export async function fetchJson(path) {
+  const response = await fetch(path);
+  const responseJson = await response.json();
+  return responseJson;
+}
+
